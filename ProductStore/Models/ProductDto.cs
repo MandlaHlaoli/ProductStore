a346220589dee0e90114732f2c4d42bb2e6b6218
HlaoliMandla <mandlahlaoli@gmail.com>
2024-07-01 06:41:42 +0200

    Initial commit

---
+++ ProductStore/Models/ProductDto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductStore.Models
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        [Required ,MaxLength(100)]
+        public string Name { get; set; } = "";
+        [Required,MaxLength(100)]
+        public string Brand { get; set; } = "";
+        [Required,MaxLength(100)]
+        public string Category { get; set; } = "";
+       
+        public decimal Price { get; set; }
+        [Required,MaxLength(100)]
+        public string Color { get; set; } = "";
+
+        public string Description { get; set; } = "";
+        [MaxLength(100)]
+        public IFormFile? ImageFile { get; set; } 
+    }
+}
